e4e35afddbaa85ae5170bf0244714a7d558d7581
Mudassir Junejo <151378976+10mudassir007@users.noreply.github.com>
2024-10-10 10:59:04 +0500

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -11,3 +11,14 @@ genre_df = movies_df['genres'].str.get_dummies("|")
 st.write(genre_df)
 
 similarity_matrix = cosine_similarity(genre_df)
+
+random_movies = movies_df['title'].sample(5)
+
+movie = st.text_input("Search for a movie")
+
+if not movie:
+  movie0 = st.button(random_movies[0])
+  movie1 = st.button(random_movies[1])
+  movie2 = st.button(random_movies[2])
+  movie3 = st.button(random_movies[3])
+  movie4 = st.button(random_movies[4])
